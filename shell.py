dd83b64c6d1733e043085ffb132a7671ab74e67b
Artyom <artemza2005@gmail.com>
2024-02-14 21:08:54 +0300

    Initial commit

---
+++ shell.py
@@ -0,0 +1,11 @@
+import basicbellang
+
+while True:
+    text = input(">")
+    result, error = basicbellang.run("<stdin1>", text)
+
+    if error:
+        print(error.as_string())
+    else:
+        print(result)
+1
